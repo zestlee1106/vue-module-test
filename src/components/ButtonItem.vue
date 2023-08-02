18f6c7d2c903e401e94df9aaf36e5104585fe5c0
이영우 <ywlee@hanatour.com>
2023-08-02 11:27:53 +0900

    ButtonItem 컴포넌트 추가

---
+++ src/components/ButtonItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <button v-bind="$attrs"><slot /></button>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+button {
+  background-color: mediumaquamarine;
+  border: 1px solid #000;
+  border-radius: 4px;
+  color: black;
+  cursor: pointer;
+  font-size: 1rem;
+  padding: 0.5rem 1rem;
+  transition: all 0.2s ease-in-out;
+}
+</style>
