f85e0b70debc350064f20856f749ddd4446225d2
이영우 <zestlee1106@gmail.com>
2023-08-02 18:16:30 +0900

    일단 커밋...

---
+++ src/components/ButtonItem.js
@@ -0,0 +1,35 @@
+import { defineComponent, h } from 'vue-demi'
+
+export default defineComponent({
+  name: 'ButtonItem',
+  props: {
+    onClick: {
+      type: Function,
+      default: () => {
+        console.log('default')
+      }
+    }
+  },
+  render(h2) {
+    if (h2 && typeof h2 === 'function') {
+      const slot = this.$slots.default
+      return h2(
+        'button',
+        {
+          onClick: this.onClick
+        },
+        slot
+      )
+    }
+
+    const slot = this.$slots.default()
+
+    return h(
+      'button',
+      {
+        onClick: this.onClick
+      },
+      slot
+    )
+  }
+})
--- vite.config.ts
+++ vite.config.ts
@@ -28,5 +28,8 @@ export default defineConfig({
         }
       }
     }
+  },
+  optimizeDeps: {
+    exclude: ['vue-demi']
   }
 })
