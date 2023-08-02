af5764605ca709cbfd7972ed021b2464ed0bd95d
이영우 <ywlee@hanatour.com>
2023-08-02 18:04:36 +0900

    패키지 배포 설정 및 vue3 용도로만 해보자

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,32 @@
 import { fileURLToPath, URL } from 'node:url'
-
+const path = require('path')
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/components/index.ts'),
+      name: 'MyLib',
+      fileName: (format) => `my-lib.${format}.js`
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // Add external deps here
+        globals: {
+          vue: 'Vue'
+        }
+      }
     }
   }
 })
