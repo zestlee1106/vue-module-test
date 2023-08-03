46580219428ad137ed781df369231278aec06501
이영우 <zestlee1106@gmail.com>
2023-08-03 17:48:28 +0900

    마지막 시도

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 const path = require('path')
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDemi from 'vue-demi'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,6 +21,8 @@ export default defineConfig({
       output: {
         globals: {
           vue: 'Vue',
+          vueDemi: 'vueDemi',
+          'vue-demi': 'vueDemi',
         }
       }
     }
