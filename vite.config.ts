b1e4eac24996bb0ea521a8b9e61b5c99b98e6969
이영우 <zestlee1106@gmail.com>
2023-08-03 15:13:41 +0900

    다시 추가

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
   optimizeDeps: {
-    exclude: ['vue-demi']
+    exclude: ["vue-demi"],
   },
   build: {
     lib: {
