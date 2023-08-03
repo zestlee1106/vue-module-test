e476910dc31b22652a57fe9f98816c0a05419948
이영우 <zestlee1106@gmail.com>
2023-08-03 18:17:34 +0900

    3 에서 안돼서 다시 수정;;

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
-import { fileURLToPath, URL } from 'node:url'
 const path = require('path')
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDemi from 'vue-demi'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -21,7 +19,6 @@ export default defineConfig({
       output: {
         globals: {
           vue: 'Vue',
-          vueDemi: 'vueDemi',
           'vue-demi': 'vueDemi',
         }
       }
